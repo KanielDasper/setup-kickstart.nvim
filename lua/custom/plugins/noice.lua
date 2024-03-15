0d9f21f9fd03b4a4295a9baf6e4901528c4136c4
KanielDasper <dkdanielkasper@gmail.com>
2024-03-15 19:37:55 +0100

    Add Neo-tree and Noice for file-nav

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,10 @@
+-- Adds a GUI commandline for NeoVim
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+}
